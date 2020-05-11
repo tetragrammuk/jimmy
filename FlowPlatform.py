b42d75c8de3f51833cd32c675b6ee207c8d0689e
tetragrammuk <bob41777@gmail.com>
2020-05-11 22:49:31 +0800

    'sfsf'

--- FlowPlatform.py
+++ FlowPlatform.py
@@ -24,6 +24,7 @@ engine_url = sqlalchemy.engine.url.URL(
     username='root',
     password='fatsheepgod',
     host='34.80.112.57',
+    # host='localhost',
     port=3306,
     database='test'
 )
@@ -1956,3 +1957,4 @@ engine.dispose()
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5001))
     app.run(ssl_context=('certificate.crt', 'private.key'),host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=port)
