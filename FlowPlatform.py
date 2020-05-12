d48bafdfd12deea260fc4af4b6ad5f47f5feee90
tetragrammuk <bob41777@gmail.com>
2020-05-12 23:30:24 +0800

    sdfsdf4565

--- FlowPlatform.py
+++ FlowPlatform.py
@@ -1960,6 +1960,6 @@ def test41():
         
 engine.dispose()
 if __name__ == "__main__":
-    # port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT', 5001))
     app.run(ssl_context=('certificate.crt', 'private.key'),host='0.0.0.0', port=port)
-    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=port)
